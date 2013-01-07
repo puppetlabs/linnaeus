eda04f28067cf179baed8346a284909b6f46da05
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-01-07 16:00:32 -0600

    make character sheet immutable after character creation

--- lib/character.rb
+++ lib/character.rb
@@ -1,9 +1,16 @@
 class Character
-  attr_accessor :character_sheet
 
-  def initialize(name = "Anonymous Character")
-    @character_sheet = {}
+  def initialize(name = "Anonymous Character", character_sheet = {})
+    @character_sheet = character_sheet
     @character_sheet[:name] = name
+  end
+
+  def character_sheet
+    # we freeze so that if someone tries to modify the copy
+    # that we gave them, they get an error rather than just
+    # letting them make a change to the copy and expecting it
+    # to affect the character sheet
+    @character_sheet.dup.freeze
   end
 
   def go_on(quest)
