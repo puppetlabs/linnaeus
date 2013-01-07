083bdf82457d5996fb0058d65f5fef0a1b33a06d
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-01-06 17:20:42 -0800

    convert character sheet to hash
    Also let npc interrogate the character, not just the character's
    treasure bag.

--- lib/character.rb
+++ lib/character.rb
@@ -1,12 +1,15 @@
 class Character
-  def character_sheet
-    []
+  attr_accessor :character_sheet
+
+  def initialize(name = "Anonymous Character")
+    @character_sheet = {}
+    @character_sheet[:name] = name
   end
 
   def go_on(quest)
     treasure = []
     quest.narrative do |npc|
-      treasure = npc.interrogate treasure
+      treasure = npc.interrogate treasure, self
     end
     treasure
   end
